ff8ef1d8e53c35af41651208052682cf1bac4946
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-12-01 15:08:26 +0100

    events

---
+++ src/Modules/Stories/src/Trill.Modules.Stories.Application/Events/External/UserCreated.cs
@@ -0,0 +1,21 @@
+using System;
+using Trill.Shared.Abstractions.Events;
+using Trill.Shared.Abstractions.Messaging;
+
+namespace Trill.Modules.Stories.Application.Events.External
+{
+    [Message("users")]
+    internal class UserCreated : IEvent
+    {
+        public Guid Id { get; set; }
+        public Guid CorrelationId { get; set; }
+        public Guid UserId { get; }
+        public string Name { get; }
+
+        public UserCreated(Guid userId, string name)
+        {
+            UserId = userId;
+            Name = name;
+        }
+    }
+}
