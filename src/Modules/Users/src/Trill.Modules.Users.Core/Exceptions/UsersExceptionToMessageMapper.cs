113ca0eba8fc29c9b7416ec0fcc785209f46980b
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-12-02 09:19:12 +0100

    exception to message mapper

--- src/Modules/Users/src/Trill.Modules.Users.Core/Exceptions/UsersExceptionToMessageMapper.cs
+++ src/Modules/Users/src/Trill.Modules.Users.Core/Exceptions/UsersExceptionToMessageMapper.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Trill.Modules.Users.Core.Domain.Exceptions;
+using Trill.Modules.Users.Core.Events;
+using Trill.Shared.Abstractions;
 using Trill.Shared.Abstractions.Events;
 using Trill.Shared.Abstractions.Exceptions;
 
@@ -8,8 +11,18 @@ namespace Trill.Modules.Users.Core.Exceptions
 {
     internal class UsersExceptionToMessageMapper : IExceptionToMessageMapper
     {
-        public IEnumerable<Type> ExceptionTypes { get; } = Enumerable.Empty<Type>();
+        public IEnumerable<Type> ExceptionTypes { get; } =
+            new[] {typeof(UserNotFoundException), typeof(InsufficientFundsException)};
 
-        public IActionRejected Map<T>(T exception) where T : Exception => null;
+        public IActionRejected Map<T>(T exception) where T : Exception
+            => exception switch
+            {
+                UserNotFoundException ex => Map(ex.UserId, ex),
+                InsufficientFundsException ex => Map(ex.UserId, ex),
+                _ => null
+            };
+
+        private static UserActionRejected Map(Guid userId, Exception exception)
+            => new UserActionRejected(userId, exception.GetExceptionCode(), exception.Message);
     }
 }
